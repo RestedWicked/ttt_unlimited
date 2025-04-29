29c672fa7881dfcae7808dac58d45c63d3f440bc
RestedWicked <wicked@radiantware.xyz>
2025-04-28 23:49:16 -0700

    [Refactor] Code is cleaner with some better algorithms

---
+++ src/lib.rs
@@ -0,0 +1,130 @@
+use core::fmt;
+use std::ops::Index;
+
+pub struct TicTacToe {
+    pub state: Vec<u8>,
+    pub grid_root: usize,
+}
+
+impl TicTacToe {
+    pub fn len(self) -> usize {
+        self.state.len()
+    }
+
+    pub fn custom_size(size: usize) -> Self {
+        let length = size * size;
+
+        TicTacToe {
+            state: vec![0; length],
+            grid_root: size,
+        }
+    }
+
+    pub fn row(&self, r: usize) -> &[u8] {
+        let start = r * self.grid_root;
+        let end = (r + 1) * self.grid_root;
+        &self.state[start..end]
+    }
+
+    pub fn col(&self, c: usize) -> Vec<u8> {
+        let mut val: Vec<u8> = Vec::new();
+        for i in 0..self.grid_root {
+            val.push(self[(i, c)]);
+        }
+        val
+    }
+
+    pub fn diag_down(&self) -> Vec<u8> {
+        let mut val: Vec<u8> = Vec::new();
+        for i in 0..self.grid_root {
+            val.push(self[(i, i)]);
+        }
+        val
+    }
+
+    pub fn diag_up(&self) -> Vec<u8> {
+        let mut val: Vec<u8> = Vec::new();
+        for (i, j) in (0..self.grid_root).zip((0..self.grid_root).rev()) {
+            val.push(self[(i, j)]);
+        }
+        val
+    }
+
+    pub fn verify_win(&self, player: u8) -> bool {
+        let win_state = vec![player; self.grid_root];
+
+        for i in 0..self.grid_root {
+            if win_state == self.row(i) {
+                return true;
+            }
+
+            if win_state == self.col(i) {
+                return true;
+            }
+        }
+
+        if win_state == self.diag_down() {
+            return true;
+        }
+
+        if win_state == self.diag_up() {
+            return true;
+        }
+
+        false
+    }
+}
+impl Default for TicTacToe {
+    fn default() -> Self {
+        TicTacToe {
+            state: vec![0; 9],
+            grid_root: 3,
+        }
+    }
+}
+
+impl Index<(usize, usize)> for TicTacToe {
+    type Output = u8;
+    fn index(&self, xy: (usize, usize)) -> &Self::Output {
+        let (x, y) = xy;
+        assert!(x < self.grid_root, "Index X was out of scope");
+        assert!(y < self.grid_root, "Index Y was out of scope");
+        let i = x + (y * self.grid_root);
+
+        &self.state[i]
+    }
+}
+
+impl Index<usize> for TicTacToe {
+    type Output = u8;
+
+    fn index(&self, i: usize) -> &Self::Output {
+        &self.state[i]
+    }
+}
+
+impl fmt::Display for TicTacToe {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let state = &self.state;
+        let side = state.len().isqrt();
+
+        for i in 0..state.len() {
+            let mut sign = ".";
+
+            if state[i] == 2 {
+                sign = "O";
+            }
+            if state[i] == 1 {
+                sign = "X";
+            }
+
+            if (i + 1) % side == 0 {
+                writeln!(f, "{}", sign)?
+            } else {
+                write!(f, "{}", sign)?
+            }
+        }
+
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,6 @@
-use std::{fmt, io::stdin, num::ParseIntError};
+use std::{io::stdin, num::ParseIntError};
 
-struct TicTacToe {
-    state: Vec<u8>,
-}
-
-impl Default for TicTacToe {
-    fn default() -> Self {
-        TicTacToe { state: vec![0; 9] }
-    }
-}
-
-impl fmt::Display for TicTacToe {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let state = &self.state;
-        let side = state.len().isqrt();
-
-        for i in 0..state.len() {
-            let mut sign = ".";
-
-            if state[i] == 2 {
-                sign = "O";
-            }
-            if state[i] == 1 {
-                sign = "X";
-            }
-
-            if (i + 1) % side == 0 {
-                writeln!(f, "{}", sign)?
-            } else {
-                write!(f, "{}", sign)?
-            }
-        }
-
-        Ok(())
-    }
-}
-
-impl TicTacToe {
-    fn custom_size(size: usize) -> Self {
-        let size = size * size;
-
-        TicTacToe {
-            state: vec![0; size],
-        }
-    }
-}
+use ttt_lib::TicTacToe;
 
 fn main() {
     game()
@@ -54,12 +10,14 @@ fn game() {
     let mut ttt = TicTacToe::default();
     println!("Welcome to TicTacToe");
     println!("{}", &ttt);
-    
+
     let mut current_player = 1;
     loop {
         println!("Your move {}: (1,9) ", current_player);
         let mut input_buffer = String::new();
-        stdin().read_line(&mut input_buffer).expect("Failed to Read Line");
+        stdin()
+            .read_line(&mut input_buffer)
+            .expect("Failed to Read Line");
 
         let player_move: Result<usize, ParseIntError> = input_buffer.trim().parse();
         if player_move.is_err() {
@@ -69,8 +27,8 @@ fn game() {
 
         let player_move = player_move.unwrap();
 
-        match player_move  {
-            1..10 => {},
+        match player_move {
+            1..10 => {}
             _ => {
                 println!("Input is not between 1-9");
                 continue;
@@ -86,7 +44,7 @@ fn game() {
 
         println!("{}", ttt);
 
-        if verify_win_condition(&ttt.state, current_player).is_some() {
+        if ttt.verify_win(current_player) {
             break;
         }
 
@@ -95,79 +53,5 @@ fn game() {
         } else {
             current_player = 1;
         }
-
-    } 
-}
-
-// Win Conditions for a 3x3 Grid
-//
-// 1 0 0 1 0 0 1 0 0 Vertical Left
-// 0 1 0 0 1 0 0 1 0 Vertical Center
-// 0 0 1 0 0 1 0 0 1 Vertical Right
-//
-// 1 1 1 0 0 0 0 0 0 Horizontal Top
-// 0 0 0 1 1 1 0 0 0 Horizontal Center
-// 0 0 0 0 0 0 1 1 1 Horizontal Bottom
-//
-// 1 0 0 0 1 0 0 0 1 Diagonal Downward
-// 1 0 0 0 - 0 1 0 0 - 0 0 1 0 - 0 0 0 1
-// 0 0 1 0 1 0 1 0 0 Diagonal Upward
-// 0 0 0 1 - 0 0 1 0 - 0 1 0 0 - 1 0 0 0
-fn verify_win_condition(game: &Vec<u8>, player: u8) -> Option<u8> {
-    // The TicTacToe grid is represented as an array of N length.
-    // If the square root of N is an integer we have a valid grid.
-    // Here I refer to it as the Grid Root.
-    let grid_root = game.len().isqrt();
-    assert_eq!(grid_root * grid_root, game.len());
-
-    // In the TicTacToe grid Players are represented as the values
-    // 1 and 2, to simplify the algorithm, I check one player at a time.
-    // The mask simply removes the other player from the grid.
-    let mut mask = vec![0; game.len()];
-    for (i, val) in game.iter().enumerate() {
-        if *val == player {
-            mask[i] = 1;
-        }
     }
-
-    let win_state = vec![1; grid_root];
-
-    let mut d_mask_down = vec![0; grid_root];
-    let mut d_mask_up = vec![0; grid_root];
-
-    // This Loop handles verifying a win state in the tic tac toe grid.
-    for i in 0..grid_root {
-        // The min and max values are used to get a row from the grid
-        // and check if it meets the win condition, each iteration of
-        // loop checks the next row.
-        let min: usize = i * grid_root;
-        let max: usize = (i + 1) * grid_root;
-        if mask[min..max] == win_state {
-            println!("You win Horizontal");
-            return Some(player);
-        }
-
-        // This one liner gets me each column to check if it meets the win
-        // condition.
-        let v_mask: Vec<_> = mask.iter().skip(i).step_by(grid_root).copied().collect();
-        if v_mask == win_state {
-            println!("You win Vertical");
-            return Some(player);
-        }
-
-        d_mask_down[i] = mask[i * (grid_root + 1)];
-        d_mask_up[i] = mask[(i + 1) * (grid_root - 1)];
-    }
-
-    if d_mask_down == win_state || d_mask_up == win_state {
-        println!("You win Diagonal");
-        return Some(player);
-    }
-
-    if !game.contains(&0) {
-        println!("Tie!");
-        return Some(3);
-    }
-
-    return None
 }
